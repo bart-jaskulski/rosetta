da3a104cd14744b6a4fadb532c911480b7d00d2a
Bart Jaskulski <bjaskulski@protonmail.com>
2023-02-26 17:44:02 +0100

    php: isbn13 validator solution
    Signed-off-by: Bart Jaskulski <bjaskulski@protonmail.com>

--- isbn13/php/src/IsbnChecker.php
+++ isbn13/php/src/IsbnChecker.php
@@ -7,6 +7,16 @@ class IsbnChecker
 
   public function isValid(string $isbn): bool
   {
-    return true;
+    $digitsArray = array_filter(
+        str_split($isbn),
+        static fn($i) => is_numeric($i)
+    );
+    return array_sum(
+      array_map(
+        static fn ($v, $k) => $k % 2 === 0 ? $v * 3 : $v,
+        $digitsArray,
+        array_keys($digitsArray)
+      )
+    ) % 10 === 0;
   }
 }
